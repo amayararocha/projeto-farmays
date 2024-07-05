804625d9e5d031ed196b4078c1339cd5dcd0493e
Mayara Rocha <mayara_rocha.pej@hotmail.com>
2024-07-05 15:08:38 -0300

    task: adicionando home na navbar

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -6,6 +6,7 @@ function Navbar() {
       <div className="container mx-auto px-4 sm:px-6 lg:px-8 flex justify-between items-center text-lg">
         <Link to='/home' className='text-2xl font-bold uppercase'>Farmays</Link>
         <div className='flex gap-3'>
+        <Link to='/home' className='hover:underline'>Home</Link>
           <Link to='/produtos' className='hover:underline'>Produtos</Link>
           <Link to='/categorias' className='hover:underline'>Categorias</Link>
           <Link to='/servicos' className='hover:underline'>Serviços</Link>
